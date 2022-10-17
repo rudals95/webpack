d1e9a0c9b89be740e15ead3c91a57599446f7756
kmjang <kmjang@nate.com>
2022-10-17 17:32:53 +0900

    r

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import { Provider } from 'react-redux';
+
 import store from '../redux/store';
 
 import 'bootstrap/dist/css/bootstrap.css';
